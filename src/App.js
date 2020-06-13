2a06e19d9070afa64300a792ae26cee946055aa2
Vishal Yadav <vishalyadav0804@gmail.com>
2020-06-13 17:09:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list';
 import './App.css';
 
 class App extends React.Component{
-  constructor(){
-    super();
+    constructor(){
+      super();
 
-  this.state={
-    monsters:[
-      {
-        name:'Frankenstein',
-        id:'1'
-      },
-      {
-        name:'Dracula',
-        id:'2'
-      },
-      {
-        name:'Zombie',
-        id:'3'
-      }
-    ]
-  };
-}
+    this.state={
+      monsters:[]
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
   render(){
     return(
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+        <CardList monsters={this.state.monsters}>
+        </CardList>
+        
       </div>
     );
   }
