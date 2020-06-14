5030bbcf8aad04ec2295becc87d2fde75a0d7f5f
Vishal Yadav <vishalyadav0804@gmail.com>
2020-06-14 17:15:59 +0530

    Updates

---
+++ precache-manifest.90e66500c7efd7274076b156e472a6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9584f235233c607844c2188cc4d39765",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2846cefbd26cd13d1d6f",
+    "url": "/monsters-rolodex/static/css/main.e2c8c5a4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2846cefbd26cd13d1d6f",
+    "url": "/monsters-rolodex/static/js/main.05345e99.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
