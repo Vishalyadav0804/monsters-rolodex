e1a2c9a2a30296c6c0a958e406d5addb90166b77
Vishal Yadav <vishalyadav0804@gmail.com>
2020-06-14 16:37:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends React.Component{
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return(
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
         placeholder='Search Monsters'
         handleChange={e => this.setState({searchField:e.target.value})}
